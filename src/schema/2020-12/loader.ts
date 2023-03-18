a8f2bd9616d8ae0dd3627a2b8a2333ad9de76fa4
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-03-18 20:28:08 +0100

    wip

--- src/schema/2020-12/loader.ts
+++ src/schema/2020-12/loader.ts
@@ -1,13 +1,29 @@
 import * as common from "../index.js";
+import { SchemaNode } from "./node.js";
+
+export interface SchemaLoaderInstanceItem {
+    instanceNode: SchemaNode;
+    instanceUrl: URL;
+    referencingInstanceUrl: URL | null;
+}
 
 export class SchemaLoader extends common.SchemaLoaderBase {
+    private readonly instanceItemMap = new Map<string, SchemaLoaderInstanceItem>();
 
     public loadFromNode(
-        schemaRootNode: unknown,
+        instanceNode: SchemaNode,
         instanceUrl: URL,
         referencingInstanceUrl: URL | null,
     ): void {
-        throw new Error("Method not implemented.");
+        const item: SchemaLoaderInstanceItem = {
+            instanceNode,
+            instanceUrl,
+            referencingInstanceUrl,
+        };
+
+        const instanceKey = String(instanceUrl);
+
+        this.instanceItemMap.set(instanceKey, item);
     }
 
 }
