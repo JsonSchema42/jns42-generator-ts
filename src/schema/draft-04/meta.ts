976627705a8120148024c1c50cb07c4a300b58c8
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-03-18 21:33:34 +0100

    wip

--- src/schema/draft-04/meta.ts
+++ src/schema/draft-04/meta.ts
@@ -2,7 +2,7 @@ import { SchemaMeta } from "../meta.js";
 import { SchemaLoader } from "./loader.js";
 import { isSchemaRootNode, SchemaNode } from "./node.js";
 
-const metaSchemaKey = "https://json-schema.org/draft-06/schema";
+const metaSchemaKey = "https://json-schema.org/draft-04/schema";
 
 export const schemaMeta: SchemaMeta<typeof metaSchemaKey, SchemaNode> = {
     metaSchemaKey,
