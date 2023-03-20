8e42a42eb37dd4114508d99246bea3ea06fbd7a7
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-03-20 10:19:33 +0100

    wip

--- src/schema/schema-draft-06/loader.ts
+++ src/schema/schema-draft-06/loader.ts
@@ -1,29 +1,13 @@
 import * as common from "../index.js";
 import { SchemaNode } from "./node.js";
 
-export interface SchemaLoaderInstanceItem {
-    instanceNode: SchemaNode;
-    instanceUrl: URL;
-    referencingInstanceUrl: URL | null;
-}
-
 export class SchemaLoader extends common.SchemaLoaderBase {
-    private readonly instanceItemMap = new Map<string, SchemaLoaderInstanceItem>();
-
     public async loadFromNode(
-        instanceNode: SchemaNode,
-        instanceUrl: URL,
-        referencingInstanceUrl: URL | null,
+        node: SchemaNode,
+        nodeUrl: URL,
+        referencingNodeUrl: URL | null,
     ): Promise<void> {
-        const item: SchemaLoaderInstanceItem = {
-            instanceNode,
-            instanceUrl,
-            referencingInstanceUrl,
-        };
-
-        const instanceKey = String(instanceUrl);
-
-        this.instanceItemMap.set(instanceKey, item);
+        throw new Error("not implemented");
     }
 
 }
