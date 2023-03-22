b725d05e90af5e5c00d5d8072560b967c97f38d2
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-03-22 17:09:12 +0100

    draft4 wip

--- src/programs/package.ts
+++ src/programs/package.ts
@@ -21,6 +21,10 @@ export function configureLabProgram(argv: yargs.Argv) {
                     type: "string",
                     choices: [
                         "https://json-schema.org/draft/2020-12/schema",
+                        // "https://json-schema.org/draft/2019-09/schema",
+                        // "https://json-schema.org/draft-07/schema",
+                        // "https://json-schema.org/draft-06/schema",
+                        "https://json-schema.org/draft-04/schema",
                     ] as const,
                     default: "https://json-schema.org/draft/2020-12/schema",
                 }).
