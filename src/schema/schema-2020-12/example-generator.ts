39fa36319713e188e697b36f01b2ec01572c2a80
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-03-24 11:37:45 +0100

    wip

--- src/schema/schema-2020-12/example-generator.ts
+++ src/schema/schema-2020-12/example-generator.ts
@@ -1,15 +1,23 @@
 import { SchemaExampleGeneratorBase } from "../example-generator.js";
 import { SchemaManager } from "../manager.js";
+import { SchemaIndexer } from "./indexer.js";
+import { SchemaLoader } from "./loader.js";
 
 export class SchemaExampleGenerator extends SchemaExampleGeneratorBase {
     constructor(
         manager: SchemaManager,
+        private readonly loader: SchemaLoader,
+        private readonly indexer: SchemaIndexer,
     ) {
         super(manager);
     }
 
     public *generateValidExamples(nodeUrl: URL): Iterable<unknown> {
-        yield* [];
+        const nodeId = String(nodeUrl);
+
+        const item = this.indexer.getNodeItem(nodeId);
+
+        yield [];
     }
 
 }
--- src/schema/schema-draft-07/example-generator.ts
+++ src/schema/schema-draft-07/example-generator.ts
@@ -1,9 +1,13 @@
 import { SchemaExampleGeneratorBase } from "../example-generator.js";
 import { SchemaManager } from "../manager.js";
+import { SchemaIndexer } from "./indexer.js";
+import { SchemaLoader } from "./loader.js";
 
 export class SchemaExampleGenerator extends SchemaExampleGeneratorBase {
     constructor(
         manager: SchemaManager,
+        private readonly loader: SchemaLoader,
+        private readonly indexer: SchemaIndexer,
     ) {
         super(manager);
     }
