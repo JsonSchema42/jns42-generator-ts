e2893a66abb5335eeef6dca4862529e50f1e491f
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-03-25 09:36:08 +0100

    wip

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,5 +3,6 @@ export * from "./object.js";
 export * from "./package.js";
 export * from "./pointer.js";
 export * from "./root.js";
+export * from "./types.js";
 export * from "./validation.js";
 
