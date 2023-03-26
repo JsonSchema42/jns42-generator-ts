4c575ef6fb6fd99df2014aaade757aaae51b3c48
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-03-26 09:32:03 +0200

    namer

--- src/schema/namer.ts
+++ src/schema/namer.ts
@@ -1,10 +1,71 @@
+import camelcase from "camelcase";
 import { SchemaManager } from "./manager.js";
 
-export abstract class SchemaNamerBase {
+export abstract class SchemaNamerBase<N> {
+    protected abstract selectSubNodeEntries(
+        nodeId: string,
+    ): Iterable<readonly [string, N]>
+
+    protected abstract selectNodeRootUrl(nodeId: string): URL | undefined
+    protected abstract selectNodePointer(nodeId: string): string | undefined
+
     constructor(
         protected readonly manager: SchemaManager,
     ) {
         //
+    }
+
+    public * getTypeNames(
+        nodeId: string,
+        baseName = "",
+    ): Iterable<readonly [string, string]> {
+        const re = /[^A-Za-z0-9]/u;
+
+        const nodeRootUrl = this.selectNodeRootUrl(nodeId);
+        if (nodeRootUrl == null) {
+            throw new Error("nodeRootUrl not found");
+        }
+
+        const nodePointer = this.selectNodePointer(nodeId);
+        if (nodePointer == null) {
+            throw new Error("nodePointer not found");
+        }
+
+        const pathParts = nodeRootUrl.pathname.
+            split("/").
+            map(decodeURI).
+            map(value => value.replace(re, ""));
+        const pointerParts = nodePointer.
+            split("/").
+            map(decodeURI).
+            map(value => value.replace(re, ""));
+
+        const nameParts = nodePointer === "" ?
+            [
+                pathParts[pathParts.length - 1],
+                pointerParts[pointerParts.length - 1],
+            ] :
+            [
+                baseName,
+                pointerParts[pointerParts.length - 1],
+            ];
+
+        const name = camelcase(nameParts, { pascalCase: true });
+
+        yield [nodeId, name] as const;
+
+        for (
+            const [subNodePointer] of
+            this.selectSubNodeEntries(nodeId)
+        ) {
+            const subNodeUrl = new URL(`#${subNodePointer}`, nodeRootUrl);
+            const subNodeId = String(subNodeUrl);
+            yield* this.getTypeNames(
+                subNodeId,
+                name,
+            );
+        }
+
     }
 
 }
--- src/schema/schema-draft-07/namer.ts
+++ src/schema/schema-draft-07/namer.ts
@@ -1,10 +1,40 @@
-import camelcase from "camelcase";
 import { SchemaManager } from "../manager.js";
 import { SchemaNamerBase } from "../namer.js";
 import { SchemaIndexer } from "./indexer.js";
+import { SchemaNode } from "./node.js";
 import { selectNodeInstanceEntries } from "./selectors.js";
 
-export class SchemaNamer extends SchemaNamerBase {
+export class SchemaNamer extends SchemaNamerBase<SchemaNode> {
+
+    protected selectSubNodeEntries(
+        nodeId: string,
+    ): Iterable<readonly [string, unknown]> {
+        const item = this.indexer.getNodeItem(nodeId);
+        if (item == null) {
+            throw new Error("node not found");
+        }
+        return selectNodeInstanceEntries(item.nodePointer, item.node);
+    }
+
+    protected selectNodeRootUrl(
+        nodeId: string,
+    ): URL | undefined {
+        const item = this.indexer.getNodeItem(nodeId);
+        if (item == null) {
+            throw new Error("node not found");
+        }
+        return item.nodeRootUrl;
+    }
+
+    protected selectNodePointer(
+        nodeId: string,
+    ): string | undefined {
+        const item = this.indexer.getNodeItem(nodeId);
+        if (item == null) {
+            throw new Error("node not found");
+        }
+        return item.nodePointer;
+    }
 
     constructor(
         manager: SchemaManager,
@@ -13,51 +43,4 @@ export class SchemaNamer extends SchemaNamerBase {
         super(manager);
     }
 
-    public * getTypeNames(
-        nodeId: string,
-        baseName = "",
-    ): Iterable<readonly [string, string]> {
-        const re = /[^A-Za-z0-9]/u;
-
-        const nodeItem = this.indexer.getNodeItem(nodeId);
-        if (nodeItem == null) {
-            throw new Error("nodeId not found");
-        }
-
-        const pathParts = nodeItem.nodeRootUrl.pathname.
-            split("/").
-            map(decodeURI).
-            map(value => value.replace(re, ""));
-        const pointerParts = nodeItem.nodePointer.
-            split("/").
-            map(decodeURI).
-            map(value => value.replace(re, ""));
-
-        const nameParts = nodeItem.nodePointer === "" ?
-            [
-                pathParts[pathParts.length - 1],
-                pointerParts[pointerParts.length - 1],
-            ] :
-            [
-                baseName,
-                pointerParts[pointerParts.length - 1],
-            ];
-
-        const name = camelcase(nameParts, { pascalCase: true });
-
-        yield [nodeId, name] as const;
-
-        for (
-            const [subNodePointer] of
-            selectNodeInstanceEntries(nodeItem.nodePointer, nodeItem.node)
-        ) {
-            const subNodeUrl = new URL(`#${subNodePointer}`, nodeItem.nodeRootUrl);
-            const subNodeId = String(subNodeUrl);
-            yield* this.getTypeNames(
-                subNodeId,
-                name,
-            );
-        }
-
-    }
 }
