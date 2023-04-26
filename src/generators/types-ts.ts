ad82751211d1f327f72b72d15d38687186a08b58
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-04-26 11:16:30 +0200

    reorg

--- src/generators/types-ts.ts
+++ src/generators/types-ts.ts
@@ -15,11 +15,116 @@ export class TypesTsCodeGenerator extends CodeGeneratorBase {
     ) {
         const typeName = this.namer.getName(nodeId).join("_");
 
-        yield this.generateSchemaTypeDeclarationStatement(
+        yield this.generateTypeDeclarationStatement(
             nodeId,
             typeName,
         );
+    }
 
+    protected generateTypeDeclarationStatement(
+        nodeId: string,
+        typeName: string,
+    ) {
+        const declaration = this.factory.createTypeAliasDeclaration(
+            [
+                this.factory.createToken(ts.SyntaxKind.ExportKeyword),
+            ],
+            typeName,
+            undefined,
+            this.generateTypeDefinition(
+                nodeId,
+            ),
+        );
+
+        const comments = this.manager.getComments(nodeId);
+        if (comments.length > 0) {
+            ts.addSyntheticLeadingComment(
+                declaration,
+                ts.SyntaxKind.MultiLineCommentTrivia,
+                "*\n" + comments,
+                true,
+            );
+        }
+
+        return declaration;
+    }
+
+    protected generateTypeDefinition(
+        nodeId: string,
+    ): ts.TypeNode {
+        const typeNodes = [...this.generateTypeDefinitionElements(nodeId)];
+        const node = typeNodes.length === 0 ?
+            this.factory.createKeywordTypeNode(ts.SyntaxKind.UnknownKeyword) :
+            this.factory.createParenthesizedType(this.factory.createUnionTypeNode(
+                typeNodes,
+            ));
+        return node;
+    }
+
+    protected *generateTypeDefinitionElements(
+        nodeId: string,
+    ): Iterable<ts.TypeNode> {
+        for (const typeDescriptor of this.manager.selectNodeTypeDescriptors(nodeId)) {
+            yield this.generateTypeDefinitionElement(typeDescriptor);
+        }
+    }
+
+    protected generateTypeDefinitionElement(
+        typeDescriptor: TypeDescriptorUnion,
+    ): ts.TypeNode {
+        switch (typeDescriptor.type) {
+            case "never":
+                return this.generateNeverTypeDefinition();
+
+            case "any":
+                return this.generateAnyTypeDefinition();
+
+            case "null":
+                return this.generateNullTypeDefinition();
+
+            case "boolean":
+                return this.generateBooleanTypeDefinition();
+
+            case "number":
+                return this.generateNumberTypeDefinition();
+
+            case "string":
+                return this.generateStringTypeDefinition();
+
+            case "tuple":
+                return this.generateTupleTypeDefinition(
+                    typeDescriptor.itemTypeNodeIds,
+                );
+
+            case "array":
+                return this.generateArrayTypeDefinition(
+                    typeDescriptor.itemTypeNodeId,
+                );
+
+            case "interface":
+                return this.generateInterfaceTypeDefinition(
+                    typeDescriptor.propertyTypeNodeIds,
+                    new Set(typeDescriptor.requiredProperties),
+                );
+
+            case "record":
+                return this.generateRecordTypeDefinition(
+                    typeDescriptor.propertyTypeNodeId,
+                );
+
+            case "union":
+                return this.generateUnionTypeDefinition(
+                    typeDescriptor.typeNodeIds,
+                );
+
+            case "intersection":
+                return this.generateIntersectionTypeDefinition(
+                    typeDescriptor.typeNodeIds,
+                );
+
+            default:
+                throw new Error("type not supported");
+        }
     }
 
     protected generateNeverTypeDefinition(): ts.TypeNode {
@@ -91,21 +196,18 @@ export class TypesTsCodeGenerator extends CodeGeneratorBase {
             ],
         );
     }
-
     protected generateUnionTypeDefinition(
         nodeIds: Array<string | boolean>,
     ) {
         const types = nodeIds.map(nodeId => this.generateTypeReferenceOrAnyOrNever(nodeId));
         return this.factory.createUnionTypeNode(types);
     }
-
     protected generateIntersectionTypeDefinition(
         nodeIds: Array<string | boolean>,
     ) {
         const types = nodeIds.map(nodeId => this.generateTypeReferenceOrAnyOrNever(nodeId));
         return this.factory.createIntersectionTypeNode(types);
     }
-
     protected generateTypeReference(
         nodeId: string,
     ) {
@@ -114,7 +216,6 @@ export class TypesTsCodeGenerator extends CodeGeneratorBase {
             this.factory.createIdentifier(typeName),
         );
     }
-
     protected generateTypeReferenceOrAnyOrNever(
         nodeId: string | boolean,
     ) {
@@ -127,113 +228,6 @@ export class TypesTsCodeGenerator extends CodeGeneratorBase {
         }
 
         return this.generateTypeReference(nodeId);
-    }
-
-    protected generateSchemaTypeDeclarationStatement(
-        nodeId: string,
-        typeName: string,
-    ) {
-        const declaration = this.factory.createTypeAliasDeclaration(
-            [
-                this.factory.createToken(ts.SyntaxKind.ExportKeyword),
-            ],
-            typeName,
-            undefined,
-            this.generateComposedTypeNode(
-                nodeId,
-            ),
-        );
-
-        const comments = this.manager.getComments(nodeId);
-        if (comments.length > 0) {
-            ts.addSyntheticLeadingComment(
-                declaration,
-                ts.SyntaxKind.MultiLineCommentTrivia,
-                "*\n" + comments,
-                true,
-            );
-        }
-
-        return declaration;
-    }
-
-    protected *generateTypeNodes(
-        nodeId: string,
-    ): Iterable<ts.TypeNode> {
-        for (const typeDescriptor of this.manager.selectNodeTypeDescriptors(nodeId)) {
-            yield this.generateTypeDefinition(nodeId, typeDescriptor);
-        }
-    }
-
-    protected generateComposedTypeNode(
-        nodeId: string,
-    ): ts.TypeNode {
-        const typeNodes = [...this.generateTypeNodes(nodeId)];
-        const node = typeNodes.length === 0 ?
-            this.factory.createKeywordTypeNode(ts.SyntaxKind.UnknownKeyword) :
-            this.factory.createParenthesizedType(this.factory.createUnionTypeNode(
-                typeNodes,
-            ));
-        return node;
-    }
-
-    protected generateTypeDefinition(
-        nodeId: string,
-        typeDescriptor: TypeDescriptorUnion,
-    ): ts.TypeNode {
-        switch (typeDescriptor.type) {
-            case "never":
-                return this.generateNeverTypeDefinition();
-
-            case "any":
-                return this.generateAnyTypeDefinition();
-
-            case "null":
-                return this.generateNullTypeDefinition();
-
-            case "boolean":
-                return this.generateBooleanTypeDefinition();
-
-            case "number":
-                return this.generateNumberTypeDefinition();
-
-            case "string":
-                return this.generateStringTypeDefinition();
-
-            case "tuple":
-                return this.generateTupleTypeDefinition(
-                    typeDescriptor.itemTypeNodeIds,
-                );
-
-            case "array":
-                return this.generateArrayTypeDefinition(
-                    typeDescriptor.itemTypeNodeId,
-                );
-
-            case "interface":
-                return this.generateInterfaceTypeDefinition(
-                    typeDescriptor.propertyTypeNodeIds,
-                    new Set(typeDescriptor.requiredProperties),
-                );
-
-            case "record":
-                return this.generateRecordTypeDefinition(
-                    typeDescriptor.propertyTypeNodeId,
-                );
-
-            case "union":
-                return this.generateUnionTypeDefinition(
-                    typeDescriptor.typeNodeIds,
-                );
-
-            case "intersection":
-                return this.generateIntersectionTypeDefinition(
-                    typeDescriptor.typeNodeIds,
-                );
-
-            default:
-                throw new Error("type not supported");
-        }
     }
 
 }
