8d9aa3a51d5b2539a0576b576e0592b49b967f0a
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-04-26 07:55:51 +0200

    adds typedescriptors

--- src/generators/types-ts.ts
+++ src/generators/types-ts.ts
@@ -1,6 +1,62 @@
 import ts from "typescript";
 import { CodeGeneratorBase } from "./code-generator-base.js";
 
+export interface NullTypeDescriptor {
+    type: "null"
+}
+
+export interface BooleanTypeDescriptor {
+    type: "boolean"
+    options?: boolean[]
+}
+
+export interface NumberTypeDescriptor {
+    type: "number"
+    numberType: "integer" | "float"
+    options?: number[]
+    minimumInclusive?: number
+    minimumExclusive?: number
+    maximumInclusive?: number
+    maximumExclusive?: number
+    multipleOf?: number
+}
+
+export interface StringTypeDescriptor {
+    type: "string"
+    options?: string[]
+    minimumLength?: number
+    maximumLength?: number
+    valuePattern?: RegExp
+}
+
+export interface InterfaceTypeDescriptor {
+    type: "interface"
+    requiredProperties: string[]
+}
+
+export interface RecordTypeDescriptor {
+    type: "record"
+    requiredProperties: string[]
+    minimumProperties?: number
+    maximumProperties?: number
+    propertyNamePattern?: RegExp
+    propertyTypeNodeId: string | boolean
+}
+
+export interface TupleTypeDescriptor {
+    type: "tuple"
+    uniqueItems?: boolean
+    propertyTypeNodeId: string | boolean
+}
+
+export interface ArrayTypeDescriptor {
+    type: "array"
+    minimumItems?: number
+    maximumItems?: number
+    uniqueItems?: boolean
+    itemTypeNodeId: string | boolean
+}
+
 export class TypesTsCodeGenerator extends CodeGeneratorBase {
 
     public * getStatements() {
@@ -98,6 +154,34 @@ export class TypesTsCodeGenerator extends CodeGeneratorBase {
     protected generateTypeNodes(
         nodeId: string,
     ): Iterable<ts.TypeNode> {
+        const type = "" as string;
+        switch (type) {
+            case "null":
+                break;
+
+            case "boolean":
+                break;
+
+            case "number":
+                break;
+
+            case "string":
+                break;
+
+            case "interface":
+                break;
+
+            case "record":
+                break;
+
+            case "tuple":
+                break;
+
+            case "array":
+                break;
+
+        }
+
         throw new Error("todo");
     }
 
