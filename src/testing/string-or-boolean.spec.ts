9350040467f49e27a5d37cecfa9e1aef7c63b7ed
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-05-07 15:51:40 +0200

    test setup

---
+++ src/testing/string-or-boolean.spec.ts
@@ -0,0 +1,8 @@
+import test from "node:test";
+import { projectRoot } from "../utils/index.js";
+
+const schema = await import(`${projectRoot}/.package/string-or-boolean/main.js`);
+
+test("string-or-boolean", () => {
+    schema.isStringorboolean({});
+});
