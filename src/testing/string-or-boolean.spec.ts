07674bf14ce553f4134788a72a60aeb6a6a40cbe
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-05-08 07:19:33 +0200

    wip

--- src/testing/string-or-boolean.spec.ts
+++ src/testing/string-or-boolean.spec.ts
@@ -1,8 +1,12 @@
+import assert from "node:assert/strict";
 import test from "node:test";
 import { projectRoot } from "../utils/index.js";
 
 const schema = await import(`${projectRoot}/.package/string-or-boolean/main.js`);
 
 test("string-or-boolean", () => {
-    schema.isStringOrBoolean({});
+    assert.equal(schema.isStringOrBoolean("hi!"), true);
+    assert.equal(schema.isStringOrBoolean(true), true);
+    assert.equal(schema.isStringOrBoolean({}), false);
+    assert.equal(schema.isStringOrBoolean(1), false);
 });
