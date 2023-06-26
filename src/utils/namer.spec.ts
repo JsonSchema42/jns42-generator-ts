44386c8975794d178c591cad5c92c92b721dca99
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-06-26 16:49:19 +0200

    testing

--- src/utils/namer.spec.ts
+++ src/utils/namer.spec.ts
@@ -2,8 +2,8 @@ import assert from "node:assert";
 import test from "node:test";
 import { Namer } from "./namer.js";
 
-test("namer", () => {
-    const namer = new Namer("-");
+test("namer 1", () => {
+    const namer = new Namer("o");
 
     namer.registerId("http://www.com/#/A");
     assert.deepStrictEqual(namer.getNames(), {
@@ -33,7 +33,7 @@ test("namer", () => {
 
     namer.registerId("http://www.com/#/C/A");
     assert.deepStrictEqual(namer.getNames(), {
-        "http://www.com/#/A": "A",
+        "http://www.com/#/A": "OA",
         "http://www.com/#/B": "B",
         "http://www.com/#/B/C": "BC",
         "http://www.com/#/A/C": "AC",
@@ -42,9 +42,9 @@ test("namer", () => {
 
     namer.registerId("http://www.com/#/A/B/C");
     assert.deepStrictEqual(namer.getNames(), {
-        "http://www.com/#/A": "A",
+        "http://www.com/#/A": "OA",
         "http://www.com/#/B": "B",
-        "http://www.com/#/B/C": "BC",
+        "http://www.com/#/B/C": "OBC",
         "http://www.com/#/A/C": "AC",
         "http://www.com/#/C/A": "CA",
         "http://www.com/#/A/B/C": "ABC",
@@ -52,9 +52,9 @@ test("namer", () => {
 
     namer.registerId("http://www.com/#/A/B/C/D/E/F");
     assert.deepStrictEqual(namer.getNames(), {
-        "http://www.com/#/A": "A",
+        "http://www.com/#/A": "OA",
         "http://www.com/#/B": "B",
-        "http://www.com/#/B/C": "BC",
+        "http://www.com/#/B/C": "OBC",
         "http://www.com/#/A/C": "AC",
         "http://www.com/#/C/A": "CA",
         "http://www.com/#/A/B/C": "ABC",
@@ -63,9 +63,9 @@ test("namer", () => {
 
     namer.registerId("http://www.com/#/X/Y/Z/D/E/F");
     assert.deepStrictEqual(namer.getNames(), {
-        "http://www.com/#/A": "A",
+        "http://www.com/#/A": "OA",
         "http://www.com/#/B": "B",
-        "http://www.com/#/B/C": "BC",
+        "http://www.com/#/B/C": "OBC",
         "http://www.com/#/A/C": "AC",
         "http://www.com/#/C/A": "CA",
         "http://www.com/#/A/B/C": "ABC",
@@ -75,14 +75,29 @@ test("namer", () => {
 
     namer.registerId("http://www.com/#/X/Y/Z/D/E/1");
     assert.deepStrictEqual(namer.getNames(), {
-        "http://www.com/#/A": "A",
+        "http://www.com/#/A": "OA",
         "http://www.com/#/B": "B",
-        "http://www.com/#/B/C": "BC",
+        "http://www.com/#/B/C": "OBC",
         "http://www.com/#/A/C": "AC",
         "http://www.com/#/C/A": "CA",
         "http://www.com/#/A/B/C": "ABC",
         "http://www.com/#/A/B/C/D/E/F": "CF",
         "http://www.com/#/X/Y/Z/D/E/F": "ZF",
         "http://www.com/#/X/Y/Z/D/E/1": "E1",
+    });
+});
+
+test("namer 2", () => {
+    const namer = new Namer("o");
+
+    namer.registerId("http://www.com/#/");
+    assert.deepStrictEqual(namer.getNames(), {
+        "http://www.com/#/": "O",
+    });
+
+    namer.registerId("http://www.com/#/A");
+    assert.deepStrictEqual(namer.getNames(), {
+        "http://www.com/#/": "O",
+        "http://www.com/#/A": "A",
     });
 });
