45ff25fa63fe44e9423d5aec7aaca8297242d9d8
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-06-26 07:22:23 +0200

    testing

--- src/utils/namer.spec.ts
+++ src/utils/namer.spec.ts
@@ -1,19 +1,43 @@
+import assert from "node:assert";
 import test from "node:test";
 import { Namer } from "./namer.js";
 
 test("namer", () => {
     const namer = new Namer(0);
 
-    namer.registerName("1", ["Good"]);
+    namer.registerName("1", ["A"]);
 
-    // assert.deepStrictEqual(namer.getName("1"), ["Good"]);
+    assert.deepStrictEqual(namer.getNames(), {
+        "1": ["A"],
+    });
 
-    namer.registerName("2", ["Good"]);
-    // assert.deepStrictEqual(namer.getName("1"), ["Good", "463749"]);
-    // assert.deepStrictEqual(namer.getName("2"), ["Good", "412991"]);
+    namer.registerName("2", ["B"]);
+    assert.deepStrictEqual(namer.getNames(), {
+        "1": ["A"],
+        "2": ["B"],
+    });
 
-    namer.registerName("3", ["Good"]);
-    // assert.deepStrictEqual(namer.getName("1"), ["Good", "463749"]);
-    // assert.deepStrictEqual(namer.getName("2"), ["Good", "412991"]);
-    // assert.deepStrictEqual(namer.getName("3"), ["Good", "862697"]);
+    namer.registerName("3", ["B, C"]);
+    assert.deepStrictEqual(namer.getNames(), {
+        "1": ["A"],
+        "2": ["B"],
+        "3": ["C"],
+    });
+
+    namer.registerName("4", ["A, C"]);
+    assert.deepStrictEqual(namer.getNames(), {
+        "1": ["A"],
+        "2": ["B"],
+        "3": ["B", "C"],
+        "4": ["A", "C"],
+    });
+
+    namer.registerName("5", ["C, A"]);
+    assert.deepStrictEqual(namer.getNames(), {
+        "1": ["A"],
+        "2": ["B"],
+        "3": ["B", "C"],
+        "4": ["A", "C"],
+        "5": ["C", "A"],
+    });
 });
